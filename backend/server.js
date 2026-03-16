5943587acac49853a2a18e0ee3d8e5a1c6c180f1
your-sudo <d70961060@gmail.com>
2026-03-16 15:35:19 +0700

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://pilkosis2-0.vercel.app"
+    
   ],
   methods: ['GET','POST','PUT','DELETE'],
   allowedHeaders: ['Content-Type','Authorization']
